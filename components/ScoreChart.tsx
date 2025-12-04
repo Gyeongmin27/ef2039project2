c73c5b73c434328f210589596d5ea5df3bec7d81
Gyeongmin27 <radix.lining@gmail.com>
2025-12-04 22:43:55 +0900

    Fix ESLint error: Escape quotes in ScoreChart component

--- components/ScoreChart.tsx
+++ components/ScoreChart.tsx
@@ -120,7 +120,7 @@ export default function ScoreChart({ scores, criticReview }: ScoreChartProps) {
           <div className="flex-1">
             <h4 className="text-lg font-semibold mb-2 text-gray-900">패션 비평가 평가</h4>
             <p className="text-gray-900 leading-relaxed font-medium italic">
-              "{displayReview}"
+              &ldquo;{displayReview}&rdquo;
             </p>
           </div>
         </div>
